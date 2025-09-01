e19d998bac67506572063debde03a1bf162d286c
StylistMercurial1130 <wrk.krthik@gmail.com>
2025-09-01 23:07:08 +0530

    bloom filter implementaiton

--- disk/bloomfilter.go
+++ disk/bloomfilter.go
@@ -2,7 +2,10 @@ package disk
 
 import (
 	"LsmStorageEngine/types"
+	"encoding/binary"
 	"math"
+
+	"github.com/spaolacci/murmur3"
 )
 
 // TODO ! create a bit vector
@@ -24,4 +27,37 @@ func NewBloomFilter(n float64, p float64) BloomFilter {
 		bitSetSize:        int(m),
 		hashFunctionCount: int(k),
 	}
+}
+
+func (bf *BloomFilter) Put(key []byte) {
+	for seed := range bf.hashFunctionCount {
+		setLocation := murmur3.Sum64WithSeed(key, uint32(seed)) % uint64(bf.bitSetSize)
+		bf.bitSet.Set(int(setLocation))
+	}
+}
+
+func (bf *BloomFilter) ContainsKey(key []byte) (bool, error) {
+	for seed := range bf.hashFunctionCount {
+		setLocation := murmur3.Sum64WithSeed(key, uint32(seed)) % uint64(bf.bitSetSize)
+
+		if isSet, err := bf.bitSet.IsSet(int(setLocation)); err != nil {
+			return false, err
+		} else if isSet == false {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+func (bf *BloomFilter) Serialize() []byte {
+	var serializedBf []byte
+	bitSet := bf.bitSet.Bytes()
+
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(len(bitSet)))
+	serializedBf = append(serializedBf, b...)
+	serializedBf = append(serializedBf, bitSet...)
+
+	return serializedBf
 }
