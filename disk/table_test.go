e8339f8febf9e8027a0f08670ffa94d1366912b1
StylistMercurial1130 <wrk.krthik@gmail.com>
2025-09-08 20:26:33 +0530

    adding table get test

--- disk/table_test.go
+++ disk/table_test.go
@@ -48,3 +48,44 @@ func TestTableCreation(t *testing.T) {
 		t.Errorf("test failed due to data dir deleting error : %s", err.Error())
 	}
 }
+
+func TestTableGet(t *testing.T) {
+	dataDir := "./data"
+	toBytes := func(str string) []byte { return []byte(str) }
+
+	_, err := os.Stat(dataDir)
+	if os.IsNotExist(err) {
+		err = os.Mkdir(dataDir, 0755)
+
+		if err != nil {
+			t.Errorf("test failed due to data dir creation error : %s", err.Error())
+		}
+	}
+
+	entries := []types.Record{
+		types.NewRecord(toBytes("k1"), toBytes("v1"), false),
+		types.NewRecord(toBytes("k2"), toBytes("v2"), false),
+		types.NewRecord(toBytes("k3"), toBytes("v3"), false),
+		types.NewRecord(toBytes("k4"), toBytes("v4"), false),
+	}
+
+	table, err := CreateNewTableToDisk(entries, dataDir)
+
+	if err != nil {
+		t.Errorf("test failed due table creation error : %s", err.Error())
+	}
+
+	rec, err := table.get([]byte("k3"))
+
+	if err != nil {
+		t.Errorf("test failed due table get error : %s", err.Error())
+	}
+
+	assert.Equal(t, rec, types.NewRecord(toBytes("k3"), toBytes("v3"), false))
+
+	err = os.RemoveAll(dataDir)
+
+	if err != nil {
+		t.Errorf("test failed due to data dir deleting error : %s", err.Error())
+	}
+}
